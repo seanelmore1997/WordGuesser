75fa0334ddc526ed726e01563c1593151b4a7f09
seanelmore1997 <seanelmore@berkeley.edu>
2025-07-10 03:37:06 +0000

    first commit

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,9 +3,48 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
+  attr_accessor :word, :guesses, :wrong_guesses
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+
+  def guess(letter)
+
+    raise ArgumentError, "Invalid guess" if letter.nil? || letter.empty? || letter !~ /^[a-zA-Z]$/
+
+    #check if the letter has been guessed before
+    return false if guesses =~ /#{letter}/i || wrong_guesses =~ /#{letter}/i
+    
+    if word =~ /#{letter}/i
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    true
+  end
+
+  def word_with_guesses
+    #iterate through the word. If a character is not in guesses, replace with a -
+    result = ""
+    word.each_char do |char|
+      if guesses.include?(char)
+        result+=char
+      else
+        result+='-'
+      end
+    end
+    result
+  end
+
+  def check_win_or_lose
+     return :lose if wrong_guesses.length >= 7
+
+     return :win if !word_with_guesses.include?("-")
+
+     return :play
   end
 
   # You can test it by installing irb via $ gem install irb
