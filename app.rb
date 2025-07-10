f3d858b0ef697e427d25924db9db436c185eec8b
seanelmore1997 <seanelmore@berkeley.edu>
2025-07-10 04:41:39 +0000

    implemented guessing features

--- app.rb
+++ app.rb
@@ -39,8 +39,16 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
+    letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      result = @game.guess(letter)
+      if result == false
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
 
@@ -51,7 +59,15 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    result = @game.check_win_or_lose
+
+    if result == :lose
+      redirect '/win'
+    elsif result == :win
+      redicted '/lose'
+    else
+      erb :show
+    end
   end
 
   get '/win' do
